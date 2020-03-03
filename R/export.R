08dd64892792ae1b23e18810f1feac57ed0e82dd
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2020-03-03 12:59:07 +0100

    Update export.R

--- R/export.R
+++ R/export.R
@@ -43,11 +43,14 @@ convert_pptx <- function(path, author, title = NULL, sub = NULL,
     unlink(file.path(out_dir, folder, recursive = TRUE, force = TRUE))
   }
 
-  #lang_return <- tryCatch(check_lang(xml), error = function(e) e)
-  #if(!is.null(lang_return$message)) {
-  #  unlink("assets", recursive = TRUE)
-  #  stop(lang_return$message)
-  #}
+  lang_return <- tryCatch(check_lang(xml), error = function(e) e)
+  lang_return$message = NULL
+  lang_return$dummy <- "a"
+                          
+  if(!is.null(lang_return$message)) {
+    unlink("assets", recursive = TRUE)
+    stop(lang_return$message)
+  }
 
   slds <- import_slide_xml(xml)
   rels <- import_rel_xml(xml)
